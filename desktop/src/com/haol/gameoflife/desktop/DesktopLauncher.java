5235db6bb797fb0c181e1813378fea07596a6793
Hans Olbrich <hans.olbrich2@gmail.com>
2020-07-15 21:33:19 +0200

    Initial commit

---
+++ desktop/src/com/haol/gameoflife/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.haol.gameoflife.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.haol.gameoflife.App;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new App(), config);
+	}
+}
